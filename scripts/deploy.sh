d2ec2d1190de4ed6ccaed191c384928c78fc7ba8
Felix Mannhardt <felix.mannhardt@sintef.no>
2019-08-20 14:40:03 +0200

    Second try fixing the deployment issue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ set -x
 
 gatsby build --prefix-paths
 
-rsync -r --delete-after --quiet --exclude=/.ssh $TRAVIS_BUILD_DIR/public/ blog.fmannhardt.de@fmannhardt.de:~/ 
+rsync -r --delete-after --quiet --exclude=/.ssh $TRAVIS_BUILD_DIR/public/ blog.fmannhardt.de@hwmails.de:~/ 
